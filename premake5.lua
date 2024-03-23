8ff50f04f55f8cce5a16b12a62e4146be4ba3d49
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:16:12 +0100

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,86 @@
+require("premake", ">=5.0.0-alpha13")
+
+workspace "TiledShading"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+
+    outputdir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}"
+
+
+    project "TiledShading"
+        location "TiledShading"
+        kind "ConsoleApp"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp",
+            "%{prj.name}/vendor/**.h",
+            "%{prj.name}/vendor/**.c",
+            "%{prj.name}/vendor/**.cpp",
+        }
+
+        excludes {
+            "%{prj.name}/vendor/imgui/**.h",
+            "%{prj.name}/vendor/imgui/**.c",
+            "%{prj.name}/vendor/imgui/**.cpp",
+        }
+
+        includedirs {
+            "%{prj.name}/src",
+            "%{prj.name}/vendor/glad/include",
+            "%{prj.name}/vendor/imgui",
+            "%{wks.relpath}/Dependencies/GLFW/include",
+        }
+
+        libdirs {
+            "%{wks.relpath}/Dependencies/GLFW/lib-vc2015",
+        }
+
+        filter {}
+
+        links {
+            "glfw3.lib",
+            "opengl32.lib",
+        }
+
+        defines {
+            
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines {
+                
+            }
+
+            prebuildcommands
+            {
+                ("{MKDIR} %{cfg.buildtarget.directory}")
+            }
+            
+            postbuildcommands
+            {
+                --("{COPY} %{wks.relpath}/Dependencies/GLFW/lib-vc2015/glfw3.dll ../bin/" .. outputdir .. "/TiledShading")
+            }
+
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            buildoptions "/MDd"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG" }
+            buildoptions "/MD"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines { "NDEBUG" }
+            buildoptions "/MD"
+            optimize "Full"
